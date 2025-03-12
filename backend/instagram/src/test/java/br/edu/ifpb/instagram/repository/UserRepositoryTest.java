49b0125647954fe1b4686f1c9159adc270ccf626
Emerson Andrey <eandrey413@gmail.com>
2025-03-12 14:35:47 -0300

    refactor: adicionando nomeclatura given_when_then nos metodos do UserRepositoryTest

--- backend/instagram/src/test/java/br/edu/ifpb/instagram/repository/UserRepositoryTest.java
+++ backend/instagram/src/test/java/br/edu/ifpb/instagram/repository/UserRepositoryTest.java
@@ -17,98 +17,84 @@ import br.edu.ifpb.instagram.model.entity.UserEntity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
-
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class UserRepositoryTest {
-	
-	@Autowired
-	private UserRepository userRepository;
 
-	@PersistenceContext
+    @Autowired
+    private UserRepository userRepository;
+
+    @PersistenceContext
     private EntityManager entityManager;
-	
-	private UserEntity user1 = new UserEntity();
-	
+
+    private UserEntity user1 = new UserEntity();
+
     @BeforeEach
-	void setUser(){
-       	user1.setFullName("andrey");
+    void setUser() {
+        user1.setFullName("andrey");
         user1.setEmail("ana@email.com.br");
         user1.setUsername("ana");
         user1.setEncryptedPassword("senha1235");
 
-        user1 = userRepository.save(user1); 
-		entityManager.flush(); 
+        user1 = userRepository.save(user1);
+        entityManager.flush();
 
-		
-	}
-
-	@Test
-	void buscarUsuarioPorNomeTest() {
-		
-		Optional<UserEntity> usuarioEncontrado = userRepository.findByUsername("ana");
-		
-        if(usuarioEncontrado.isPresent()){
-            assertEquals("ana", usuarioEncontrado.get().getUsername());
-		    assertEquals("ana@email.com.br",usuarioEncontrado.get().getEmail());
-        }	
-			
-		
-	}
+    }
 
     @Test
-    void buscarUsuarioComNomeNullTest(){
+    void givenUserName_whenFind_thenReturnUser() {
+
+        Optional<UserEntity> usuarioEncontrado = userRepository.findByUsername("ana");
+
+        if (usuarioEncontrado.isPresent()) {
+            assertEquals("ana", usuarioEncontrado.get().getUsername());
+            assertEquals("ana@email.com.br", usuarioEncontrado.get().getEmail());
+        }
+
+    }
+
+    @Test
+    void givenUserNull_whenFind_then_ReturnNullUser() {
         Optional<UserEntity> user = userRepository.findByUsername(null);
         assertTrue(user.isEmpty());
     }
 
+    @Test
+    void givenUser_whenFindAll_thenReturnListUsers() {
 
-	@Test
-	void retornarListaDeUsuariosExistenteTest(){
+        List<UserEntity> listaUsuarios = userRepository.findAll();
 
-		List<UserEntity> listaUsuarios = userRepository.findAll();
+        assertNotNull(listaUsuarios);
+        assertThat(listaUsuarios).extracting(UserEntity::getUsername)
+                .containsExactlyInAnyOrder("ana");
 
-		assertNotNull(listaUsuarios);
-		assertThat(listaUsuarios).extracting(UserEntity::getUsername)
-    	.containsExactlyInAnyOrder("ana");
-		
-	}
+    }
 
+    @Test
+    void givenUser_whenUpdate_thenReturnUpdatedUser() {
 
-	@Test
-    void updatePartialUserTest() {
-		System.out.println(user1.getFullName());
-		System.out.println(user1.getEmail());
-		System.out.println(user1.getUsername());
-		
         Optional<UserEntity> existeUser = userRepository.findById(user1.getId());
         assertTrue(existeUser.isPresent(), "Usuário não foi salvo corretamente!");
 
         int updatedRows = userRepository.updatePartialUser(
-                "ismael", 
-                "ismael@gmail.com", 
-                "andrey", 
-                null, 
-                user1.getId()
-        );
+                "ismael",
+                "ismael@gmail.com",
+                "andrey",
+                null,
+                user1.getId());
 
         entityManager.flush();
-        entityManager.clear(); 
+        entityManager.clear();
 
         Optional<UserEntity> updatedUserO = userRepository.findById(user1.getId());
         assertTrue(updatedUserO.isPresent(), "Usuário atualizado não encontrado!");
 
-
         UserEntity updatedUser = updatedUserO.get();
 
-		System.out.println(updatedUser.getFullName());
-		System.out.println(updatedUser.getEmail());
-		System.out.println(updatedUser.getUsername());
 
-      
         assertEquals("ismael", updatedUser.getFullName());
         assertEquals("ismael@gmail.com", updatedUser.getEmail());
-        assertEquals("andrey", updatedUser.getUsername()); 
+        assertEquals("andrey", updatedUser.getUsername());
         assertEquals("senha1235", updatedUser.getEncryptedPassword());
 
         assertEquals(1, updatedRows);
